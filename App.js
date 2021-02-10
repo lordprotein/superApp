596cb4c54b6df5fd742cf1b62c79fb28e63c86d1
lordprotein <romanov.i@mxm.email>
2021-02-10 22:41:01 +0300

    init project

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { Text } from "react-native";
+import { AboutAppScene } from "./src/scenes/aboutApp";
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
 
-  return (<Text>1</Text>);
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={ "About App" } component={ AboutAppScene }/>
+      </Tab.Navigator>
+    </NavigationContainer>);
 };
 
 export default App;
---
+++ src/scenes/aboutApp.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const AboutAppScene = () => {
+  return (<View><Text>123321</Text></View>);
+};
