305258e776e7c3bfaff2f9ef6b742af070c1c822
lordprotein <romanov.i@mxm.email>
2021-02-11 22:26:34 +0300

    progress navigation

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { Text } from "react-native";
-import { AboutAppScene } from "./src/scenes/aboutApp";
-
-const Tab = createBottomTabNavigator();
+import { Tab } from "navigation/create";
+import { mainScenesList } from "navigation/setNavs";
 
 const App = () => {
-
+  //Not good using key from index. Better use uniq id from back or generate stable id
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={ "About App" } component={ AboutAppScene }/>
+      <Tab.Navigator initialRouteName={ "About App" }>
+        { mainScenesList.map((navProps, key) => <Tab.Screen { ...navProps } key={ key }/>) }
       </Tab.Navigator>
     </NavigationContainer>);
 };
---
+++ src/navigation/setNavs.js
@@ -0,0 +1,13 @@
+import { AboutAppScene } from "scenes/aboutApp";
+import { FinQuoteScene } from "scenes/finQuoteScene";
+
+export const mainScenesList = [
+  {
+    component: AboutAppScene,
+    name: "About App",
+  },
+  {
+    component: FinQuoteScene,
+    name: "Finance Quote",
+  },
+];
